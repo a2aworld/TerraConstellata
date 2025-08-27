a2fde75b5aa8c67f4a29f77e87e5e9b111148030
A2A World <om@a2aworld.ai>
2025-08-27 19:13:06 -0400

    Update main.ts

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,18 +1,9 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Terra Constellata</title>
-    <link rel="stylesheet" href="./style.css" />
-  </head>
-  <body>
-    <div id="app-container">
-      <iframe
-        src="https://earth.google.com/earth/d/1ArKI0ipcOJ3QEXvDyNte8MLd7WfXbYqO?usp=sharing"
-        title="Google Earth Project"
-      ></iframe>
-    </div>
-    <script type="module" src="./main.ts"></script>
-  </body>
-</html>
+import { navigateTo } from '@devvit/client';
+
+const docsLink = document.getElementById('docs-link');
+const playtestLink = document.getElementById('playtest-link');
+const discordLink = document.getElementById('discord-link');
+
+docsLink?.addEventListener('click', () => navigateTo('https://developers.reddit.com/docs'));
+playtestLink?.addEventListener('click', () => navigateTo('https://www.reddit.com/r/Devvit'));
+discordLink?.addEventListener('click', () => navigateTo('https://discord.com/invite/R7yu2wh9Qz'));
